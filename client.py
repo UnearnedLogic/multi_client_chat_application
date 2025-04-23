c3d89ffd02804dc8e51cfed494a4506b53a9fe87
UnearnedLogic <ruben.pogosyan.95@gmail.com>
2025-04-24 00:57:10 +0400

    add description

--- client.py
+++ client.py
@@ -1,44 +1,49 @@
 import socket
 import threading
 
+# Prompt the user to choose a nickname
 nickname = input("Choose your nickname: ")
 
-host = '127.0.0.1'
-port = 9090
+# Server connection settings
+host = '127.0.0.1'  # Server's IP address
+port = 9090         # Server's port number
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host,port))
+# Create and connect the client's socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Socket for TCP connection
+client.connect((host, port))  # Connect to the server
 
 
-
+# Function to receive messages from the server
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+            message = client.recv(1024).decode('ascii')  # Receive and decode a message from the server
+            if message == 'NICK':  # If the server asks for a nickname
+                client.send(nickname.encode('ascii'))  # Send the user's chosen nickname
             else:
-                print(message)
+                print(message)  # Print any other messages from the server
         except:
+            # Handle unexpected disconnection from the server
             print("Disconnected from the server!")
-            client.close()
+            client.close()  # Close the connection
             break
 
+# Function to send messages to the server
 def write():
     while True:
-        text = input("")
-        message = f'{nickname}: {text}'
-        if text == '/quit':
-            client.send('/quit'.encode('ascii'))
-            client.close()
+        text = input("")  # Get user input
+        message = f'{nickname}: {text}'  # Format the message with the user's nickname
+        if text == '/quit':  # If the user types the quit command
+            client.send('/quit'.encode('ascii'))  # Notify the server
+            client.close()  # Close the connection
             break
         else:
-            client.send(message.encode('ascii'))
+            client.send(message.encode('ascii'))  # Otherwise, send the message to the server
 
-
+# Start a thread to handle receiving messages
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-
+# Start a thread to handle sending messages
 write_thread = threading.Thread(target=write)
 write_thread.start()
--- server.py
+++ server.py
@@ -1,57 +1,68 @@
 import socket
 import threading
 
-host = '127.0.0.1'
-port = 9090
+# Server setup
+host = '127.0.0.1'  # Localhost IP address
+port = 9090         # Port number to listen on
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host,port))
-server.listen()
+# Create and configure the server socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Socket for TCP connection
+server.bind((host, port))  # Bind the server to the specified host and port
+server.listen()  # Start listening for incoming connections
 
+# Lists to store connected clients and their nicknames
 clients = []
 nicknames = []
 
+# Function to broadcast messages to all connected clients
 def broadcast(message):
-    for client in clients:
-        client.send(message)
+    for client in clients:  # Iterate through all connected clients
+        client.send(message)  # Send the message to each client
 
-
+# Function to handle communication with a specific client
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            decoded_message = message.decode('ascii')
-            if decoded_message == '/quit':
-                index = clients.index(client)
-                nickname = nicknames[index]
-
+            message = client.recv(1024)  # Receive a message from the client
+            decoded_message = message.decode('ascii')  # Decode the message
+            if decoded_message == '/quit':  # Check if the client wants to disconnect
+                index = clients.index(client)  # Get the index of the client
+                nickname = nicknames[index]  # Retrieve the client's nickname
+                
+                # Remove the client and their nickname from the lists
                 clients.remove(client)
                 nicknames.remove(nickname)
 
+                # Notify everyone about the client's disconnection
                 broadcast(f'{nickname} left the chat!'.encode('ascii'))
-                client.close()
+                client.close()  # Close the client's connection
                 break
             else:
-                broadcast(message)  # send bytes directly
+                # Broadcast the message to all connected clients
+                broadcast(message)  # Send bytes directly
         except:
-            # handle disconnect
+            # Handle unexpected disconnection
             pass
 
+# Function to accept and manage incoming connections
 def receive():
     while True:
-        client, address = server.accept()
-        print(f"Connected with {str(address)}")
+        client, address = server.accept()  # Accept a new client connection
+        print(f"Connected with {str(address)}")  # Print the client's address
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+        # Request the client's nickname
+        client.send('NICK'.encode('ascii'))  # Send 'NICK' to request a nickname
+        nickname = client.recv(1024).decode('ascii')  # Receive and decode the nickname
+        nicknames.append(nickname)  # Add the client's nickname to the list
+        clients.append(client)  # Add the client's socket to the list
 
-        print(f"Nickname of the client is {nickname}")
-        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        # Notify all users about the new connection
+        print(f"Nickname of the client is {nickname}")  # Print the client's nickname
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))  # Notify all about the new user
+        client.send('Connected to the server!'.encode('ascii'))  # Send a welcome message to the client
 
+        # Start a thread to handle the new client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-receive()
+receive()  # Run the server's receive function
